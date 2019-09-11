7f71b3ca8d1fdcc91ca460c2a9a84079fa9d26b1
lowell1 <lowelljacobs@mail.com>
2019-09-11 15:42:52 -0700

    new branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
+import react from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
     </div>
   );
 }
